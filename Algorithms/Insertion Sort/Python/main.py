f1efc83efa5a05859d6d9430c9e4d1b73d44152a
Rumata888 <isennovskiy@gmail.com>
2013-08-11 02:11:34 +0400

    finalized insertion sort in python, moving on to peak algorithms in mit

--- Algorithms/Insertion Sort/Python/main.py
+++ Algorithms/Insertion Sort/Python/main.py
@@ -1,6 +1,7 @@
 class InsertionSort:
     @staticmethod
-    def Sort(a,b):
+    def Sort(a):
+         b=len(a)
          c=1
          while c<b:
              d=c-1
@@ -13,12 +14,27 @@ class InsertionSort:
                     break
              a[d+1]=k;
              c=c+1;
+         return a;
+     
+class FileWork:
+    @staticmethod
+    def LoadArray(a):
+        f=open(a,'r');
+        array=[];
+        for line in f:
+            array=array+[int(line)]
+        f.close()
+        return array;
+    @staticmethod
+    def SaveArray(a,b):
+        f=open(a,'w+')
+        for line in b:
+            f.write(str(line)+"\n")
+        f.close()
 
-
-a=[3,2,1]
-b=3
-InsertionSort.Sort(a, b)
-print(a)
+a=FileWork.LoadArray("sequence.txt")
+a=InsertionSort.Sort(a)
+FileWork.SaveArray("output.txt", a)
         
          
         
